986301718be37164e4159e0763d91bbf58e2203d
chumpon-asaneerat <chumponsenate@gmail.com>
2020-09-26 10:59:08 +0700

    - Update DMT.TA.TODv1 #28   - DMT Controls project.     - Add Header related controls.

---
+++ 05.Controls/DMT.Controls/Header/CurrentTA.cs
@@ -0,0 +1,27 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+//using NLib.Services;
+using DMT.Models;
+using DMT.Services;
+
+#endregion
+
+
+namespace DMT.Controls
+{
+    public static class TAApp
+    {
+        // TODO: Refactor
+        /*
+        public static class User
+        {
+            public static Models.User Current { get; set; }
+        }
+        */
+    }
+}
