8a6af42ec24fed240d2fb83a25dab5af8ec8b194
chumpon-asaneerat <chumponsenate@gmail.com>
2020-10-06 10:06:54 +0700

    Ported DMTv2 to DMT.TAxTODv1 #107 - Update RouteConts.cs #33   - Update DMT.RestServer project #3.

--- 03.WebServices/DMT.RestServer/Services/DMTRestServerStartUp.cs
+++ 03.WebServices/DMT.RestServer/Services/DMTRestServerStartUp.cs
@@ -1,10 +1,24 @@
 ﻿#region Using
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reflection;
+// Owin SelfHost
+using Owin;
+using Microsoft.Owin; // for OwinStartup attribute.
+using Microsoft.Owin.Hosting;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Validation;
+// Owin Authentication
+using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Text;
+// Swagger
+using Swashbuckle.Application;
+using NLib;
 
 #endregion
 
@@ -15,5 +29,75 @@ namespace DMT.Services
     /// </summary>
     public abstract class DMTRestServerStartUp
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DMTRestServerStartUp() : base()
+        {
+            // Authentication
+            this.HasAuthentication = true;
+            this.AuthenticationSchemes = AuthenticationSchemes.Basic |
+                //AuthenticationSchemes.IntegratedWindowsAuthentication |
+                AuthenticationSchemes.Anonymous;
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Init Authentication.
+        /// </summary>
+        /// <param name="app">The IAppBuilder instance.</param>
+        protected virtual void InitAuthentication(IAppBuilder app)
+        {
+            if (null == app) return;
+            if (!HasAuthentication) return;
+            // Setup Authentication for listener.
+            HttpListener listener;
+            MethodBase med = MethodBase.GetCurrentMethod();
+            try
+            {
+                listener = app.Properties["System.Net.HttpListener"] as HttpListener;
+                if (null != listener)
+                {
+                    listener.AuthenticationSchemes = AuthenticationSchemes;
+                }
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Configuration.
+        /// </summary>
+        /// <param name="app">The IAppBuilder instance.</param>
+        public virtual void Configuration(IAppBuilder app)
+        {
+            InitAuthentication(app);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or (protected sets) has authentication.
+        /// </summary>
+        public bool HasAuthentication { get; protected set; }
+        /// <summary>
+        /// Gets or (protected sets) Authentication Schemes.
+        /// </summary>
+        public AuthenticationSchemes AuthenticationSchemes { get; protected set; }
+
+        #endregion
     }
 }
