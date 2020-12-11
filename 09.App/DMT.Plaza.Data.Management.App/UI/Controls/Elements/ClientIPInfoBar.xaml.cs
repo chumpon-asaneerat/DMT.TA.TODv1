6539abff660bfbc73134b73983029bdf7f10d69e
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-11 11:10:19 +0700

    Ported DMTv2 to DMT.TAxTODv1 #232 - DMT.TAxTODv1 Rework #7   - DMT.Plaza.Data.Management.App project.     - Add Statusbar Elements (Control) -> ClientIPInfoBar.     - Update Statusbar Control -> add IP address.

---
+++ 09.App/DMT.Plaza.Data.Management.App/UI/Controls/Elements/ClientIPInfoBar.xaml.cs
@@ -0,0 +1,47 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+using NLib.Utils;
+
+#endregion
+
+namespace DMT.UI.Controls.Elements
+{
+    /// <summary>
+    /// Interaction logic for ClientIPInfoBar.xaml
+    /// </summary>
+    public partial class ClientIPInfoBar : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ClientIPInfoBar()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var localIP = NetworkUtils.GetLocalIPAddress();
+            txtIPAddress.Text = (null != localIP) ? localIP.ToString() : "0.0.0.0";
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
