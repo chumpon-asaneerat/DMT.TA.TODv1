8cfb1811c63a991467ae5bdb52a5614d2491e069
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-13 20:42:34 +0700

    Ported DMTv2 to DMT.TAxTODv1 #241 - DMT.TAxTODv1 Rework #16   - Add Wpf.ContentPresenter.Sample Project.

---
+++ 10.Tests/Wpf.ContentPresenter.Sample/Controls/MetroUIMainMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.ContentPresenter.Sample.Controls
+{
+    /// <summary>
+    /// Interaction logic for MetroUIMainMenu.xaml
+    /// </summary>
+    public partial class MetroUIMainMenu : UserControl
+    {
+        public MetroUIMainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
