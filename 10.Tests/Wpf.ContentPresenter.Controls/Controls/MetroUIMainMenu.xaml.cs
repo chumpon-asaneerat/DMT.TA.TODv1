e9a6bcd4a6ae8e795a882a3ee2b7ffadec0c4de0
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-14 18:40:20 +0700

    Ported DMTv2 to DMT.TAxTODv1 #244 - DMT.TAxTODv1 Rework #19   - Update Wpf.ContentPresenter.Controls Project #2.   - Update Wpf.ContentPresenter.Sample Project #2.

--- 10.Tests/Wpf.ContentPresenter.Controls/Controls/MetroUIMainMenu.xaml.cs
+++ 10.Tests/Wpf.ContentPresenter.Controls/Controls/MetroUIMainMenu.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Wpf.ContentPresenter.Sample.Controls
+namespace Wpf.ContentPresenter.Controls
 {
     /// <summary>
     /// Interaction logic for MetroUIMainMenu.xaml
