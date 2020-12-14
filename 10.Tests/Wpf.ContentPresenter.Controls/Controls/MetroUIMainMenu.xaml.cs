7d85acaf92c9fd4d13bdb1519e4aff31430ad0b3
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-14 19:28:56 +0700

    Ported DMTv2 to DMT.TAxTODv1 #245 - DMT.TAxTODv1 Rework #20   - Update Wpf.ContentPresenter.Controls Project #3.   - Update Wpf.ContentPresenter.Sample Project #3.

--- 10.Tests/Wpf.ContentPresenter.Controls/Controls/MetroUIMainMenu.xaml.cs
+++ 10.Tests/Wpf.ContentPresenter.Controls/Controls/MetroUIMainMenu.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -18,11 +20,24 @@ namespace Wpf.ContentPresenter.Controls
     /// <summary>
     /// Interaction logic for MetroUIMainMenu.xaml
     /// </summary>
+    [ContentProperty("Groups")]
     public partial class MetroUIMainMenu : UserControl
     {
         public MetroUIMainMenu()
         {
             InitializeComponent();
+
+            groupItemsControl.DataContext = this;
+            Groups = new ObservableCollection<MetroUIGroupMenu>();
+        }
+
+        public static readonly DependencyProperty GroupsProperty =
+                DependencyProperty.Register("Groups", typeof(ObservableCollection<MetroUIGroupMenu>), typeof(MetroUIGroupMenu), new PropertyMetadata(null));
+
+        public ObservableCollection<MetroUIGroupMenu> Groups
+        {
+            get { return (ObservableCollection<MetroUIGroupMenu>)GetValue(GroupsProperty); }
+            set { SetValue(GroupsProperty, value); }
         }
     }
 }
