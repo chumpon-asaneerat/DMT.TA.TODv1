89933673621003a03871531bf60b1c3433d8977c
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-16 16:06:08 +0700

    Ported DMTv2 to DMT.TAxTODv1 #274 - DMT.TAxTODv1 Rework #43   - DMT.RestClient project.     - Add RestAPI.cs

---
+++ 03.WebServices/DMT.RestClient/Services/RestAPI.cs
@@ -0,0 +1,43 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+using RestSharp;
+using NLib.ServiceProcess;
+
+using DMT.Models;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+#endregion
+
+namespace DMT.Services
+{
+    public static partial class RestAPI
+    {
+        public static partial class Plaza { }
+        public static partial class SCW { }
+        public static partial class TAxTOD { }
+        public static partial class TAApp { }
+        public static partial class TODApp { }
+    }
+
+    static partial class RestAPI
+    {
+        static partial class Plaza 
+        {
+            public static void Omega() { }
+        }
+    }
+
+    public class XXX
+    {
+        public void Test()
+        {
+            RestAPI.Plaza.Omega();
+        }
+    }
+}
