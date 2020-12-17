fffd7588ab94d00d3b79c8c9a8230d6ed6126d5f
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-17 12:00:38 +0700

    Ported DMTv2 to DMT.TAxTODv1 #284 - DMT.TAxTODv1 Rework #52   - Update RestAPI design todo file(s).   - DMT.Core project.     - Remove RouteConsts.cs     - Restucture RouteConsts code.       - Add RouteConsts.cs -> base class.       - Client routes         - Add Client.Controller.cs         - Add Register.cs (action route).         - Add Unregister.cs (action route).       - Notify routes         - Add Notify.Controller.cs         - Add TSBChanged.cs (action route).         - Add ShiftChanged.cs (action route).       - Infrastructure routes         - Add Infrastructure.Controller.cs (for all TSB/PlazaGroup/Plaza/Lane base class).         - TSB routes           - Add Gets.cs (action route).           - Add Current.cs (action route).           - Add SetActive.cs (action route).           - Add Save.cs (action route).         - PlazaGroup routes           - Add Gets.cs (action route).           - Add Save.cs (action route).         - Plaza routes           - Add Gets.cs (action route).           - Add Save.cs (action route).         - Lane routes           - Add Gets.cs (action route).           - Add Save.cs (action route).

---
+++ 01.Core/DMT.Core/Consts/Routes/Infrastructure/Infrastructure.Controller.cs
@@ -0,0 +1,55 @@
+﻿namespace DMT
+{
+    // Url: api/infrastructure
+    static partial class RouteConsts
+    {
+        /// <summary>The Infrastructure Controller.</summary>
+        public static partial class Infrastructure
+        {
+            /// <summary>Gets controller name.</summary>
+            public const string ControllerName = "Infrastructure";
+            /// <summary>Gets base controller url.</summary>
+            public const string Url = RouteConsts.Url + @"/" + ControllerName;
+
+            // Url : api/infrastructure/tsb
+            /// <summary>The Infrastructure's TSB class.</summary>
+            public static partial class TSB 
+            {
+                /// <summary>Gets route name.</summary>
+                public const string Name = "TSB";
+                /// <summary>Gets route url.</summary>
+                public const string Url = Infrastructure.Url + @"/" + Name;
+            }
+
+            // Url : api/infrastructure/plazagroup
+            /// <summary>The Infrastructure's PlazaGroup class.</summary>
+            public static partial class PlazaGroup 
+            {
+                /// <summary>Gets route name.</summary>
+                public const string Name = "PlazaGroup";
+                /// <summary>Gets route url.</summary>
+                public const string Url = Infrastructure.Url + @"/" + Name;
+            }
+
+            // Url : api/infrastructure/plaza
+            /// <summary>The Infrastructure's Plaza class.</summary>
+            public static partial class Plaza 
+            {
+                /// <summary>Gets route name.</summary>
+                public const string Name = "Plaza";
+                /// <summary>Gets route url.</summary>
+                public const string Url = Infrastructure.Url + @"/" + Name;
+            }
+
+            // Url : api/infrastructure/lane
+            /// <summary>The Infrastructure's Lane class.</summary>
+            public static partial class Lane 
+            {
+                /// <summary>Gets route name.</summary>
+                public const string Name = "Lane";
+                /// <summary>Gets route url.</summary>
+                public const string Url = Infrastructure.Url + @"/" + Name;
+            }
+        }
+    }
+}
