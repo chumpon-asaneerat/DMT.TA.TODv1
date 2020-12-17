d723087ac3be64db0be64ac443b1fbee4b3f2ddb
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-17 09:11:22 +0700

    Ported DMTv2 to DMT.TAxTODv1 #279 - DMT.TAxTODv1 Rework #48   - DMT.Core project.     - Add LocalServiceManager.cs for install/uninstall/checkstatus of windows service.

--- 03.WebServices/DMT.RestClient/Services/Local/Local.cs
+++ 03.WebServices/DMT.RestClient/Services/Local/Local.cs
@@ -10,5 +10,28 @@ namespace DMT.Services.Operations
     /// <summary>
     /// The Plaza Operation class.
     /// </summary>
-    public static partial class Plaza { }
+    public static partial class Plaza 
+    {
+        #region Static Properties
+
+        /// <summary>
+        /// Gets Base Address.
+        /// </summary>
+        public static string BaseAddress
+        {
+            get
+            {
+                if (null == ConfigManager.Instance.Plaza) return string.Empty;
+                if (null == ConfigManager.Instance.Plaza.Local) return string.Empty;
+                if (null == ConfigManager.Instance.Plaza.Local.Service) return string.Empty;
+
+                return string.Format(@"{0}://{1}:{2}/",
+                    ConfigManager.Instance.Plaza.Local.Service.Protocol,
+                    ConfigManager.Instance.Plaza.Local.Service.HostName,
+                    ConfigManager.Instance.Plaza.Local.Service.PortNumber);
+            }
+        }
+
+        #endregion
+    }
 }
