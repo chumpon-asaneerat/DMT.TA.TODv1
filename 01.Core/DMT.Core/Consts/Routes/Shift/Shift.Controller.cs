23c8b00873a8e5329e9e967d7ba53dc32906b157
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-24 10:02:51 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #397 - DMT.TAxTODv1   - DMT.Core project.     - Add Routes Controller       - Shift       - Security -> Role       - Security -> User     - Add Routes Action Url       - Shift -> Gets

---
+++ 01.Core/DMT.Core/Consts/Routes/Shift/Shift.Controller.cs
@@ -0,0 +1,15 @@
+﻿namespace DMT
+{
+    // Url: api/Shift
+    static partial class RouteConsts
+    {
+        /// <summary>The Shift Controller.</summary>
+        public static partial class Shift
+        {
+            /// <summary>Gets controller name.</summary>
+            public const string ControllerName = "Shift";
+            /// <summary>Gets base controller url.</summary>
+            public const string Url = RouteConsts.Url + @"/" + ControllerName;
+        }
+    }
+}
