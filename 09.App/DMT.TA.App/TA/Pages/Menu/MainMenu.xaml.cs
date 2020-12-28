8e58c020943d378e4291a67eb7b50e548034a6c7
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-28 16:15:29 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #498 - DMT.TAxTODv1   - DMT.TA.App Project     - MainMenu button handlers (Update and Recheck)       - Request Exchange -> RequestExchangePage.       - Returns Exchange -> ManageExchangePage.       - Internal Exchange -> InternalExchangePage.       - Chief Sale Coupon -> CouponTSBSalePage.       - Coupon Sale History -> CouponHistoryViewPage.       - Credit History Report -> need to recheck DMTv2 report image.       - Collector Credit Manage (Receive/Returns) -> CollectorCreditManagePage.       - Collector Receive Coupon -> ReceiveCouponPage.       - Collector Returns Coupon -> ReturnCouponPage.       - Collector Credit History -> CreditHistoryViewPage.       - Plaza Check Balance -> PlazaBalanceSummaryWindow.

--- 09.App/DMT.TA.App/TA/Pages/Menu/MainMenu.xaml.cs
+++ 09.App/DMT.TA.App/TA/Pages/Menu/MainMenu.xaml.cs
@@ -32,56 +32,92 @@ namespace DMT.TA.Pages.Menu
         private void cmdRequestExchange_Click(object sender, RoutedEventArgs e)
         {
             // ยืม/แลก เงินยืมทอนฝ่ายบัญชี
+            var page = new Exchange.RequestExchangePage();
+            //page.Setup();
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdReturnExchange_Click(object sender, RoutedEventArgs e)
         {
             // คืนเงินยืมทอนฝ่ายบัญชี
+            var page = new Exchange.ManageExchangePage();
+            //page.Setup();
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdInHouseExchange_Click(object sender, RoutedEventArgs e)
         {
             // แลกเงินหมุนเวียนในด่าน
+            var page = new Exchange.InternalExchangePage();
+            //page.Setup();
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdCouponSoldByPlaza_Click(object sender, RoutedEventArgs e)
         {
             // หัวหน่าขายคูปอง
+            var page = new Coupon.CouponTSBSalePage();
+            //page.Setup(DMT.Controls.TAApp.User.Current);
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdCouponSoldHistory_Click(object sender, RoutedEventArgs e)
         {
             // ประวัติการขายคูปอง
+            var page = new Coupon.CouponHistoryViewPage();
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdCreditTransactionSummaryReport_Click(object sender, RoutedEventArgs e)
         {
             // รายงานสรุปการยืมเงินทอน
+            /*
+            var page = new Reports.CollectorFundSummaryReportPage();
+            page.Setup(DMT.Controls.TAApp.User.Current);
+            PageContentManager.Instance.Current = page;
+            */
         }
 
         private void cmdUserCreditManage_Click(object sender, RoutedEventArgs e)
         {
             // เงินยืมทอน (collector)
+            var page = new Credit.CollectorCreditManagePage();
+            //page.RefreshPlazaInfo();
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdUserBorrowCoupon_Click(object sender, RoutedEventArgs e)
         {
             // รับคูปอง (collector)
+            var page = new Coupon.ReceiveCouponPage();
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdUserReturnCoupon_Click(object sender, RoutedEventArgs e)
         {
             // คืนคูปอง (collector)
+            var page = new Coupon.ReturnCouponPage();
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdUserCreditHistory_Click(object sender, RoutedEventArgs e)
         {
             // ประวัติการแลกเงินยืมทอน (collector)
+            var page = new Credit.CreditHistoryViewPage();
+            //page.Setup(DMT.Controls.TAApp.User.Current);
+            PageContentManager.Instance.Current = page;
         }
 
         private void cmdCheckBalance_Click(object sender, RoutedEventArgs e)
         {
             // เช็คยอดด่าน
+            var win = new Windows.Plaza.PlazaBalanceSummaryWindow();
+            win.Owner = Application.Current.MainWindow;
+            //win.RefreshPlazaInfo();
+            if (win.ShowDialog() == false)
+            {
+                return;
+            }
         }
 
         private void cmdExit_Click(object sender, RoutedEventArgs e)
@@ -98,6 +134,7 @@ namespace DMT.TA.Pages.Menu
                 "MT_ADMIN", "MT_TECH",
                 "FINANCE", "SV",
                 "RAD_MGR", "RAD_SUP");
+
             PageContentManager.Instance.Current = page;
         }
 
