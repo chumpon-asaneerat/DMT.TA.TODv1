dfc1606a62dd93304c1abe1a9edd129987a09407
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-30 12:53:12 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #528 - DMT.TAxTODv1   - DMT.TA.App project.     - InternalExchangePage add button handlers code.     - ManageExchangePage add button handlers code.     - RequestExchangePage add button handlers code.

--- 09.App/DMT.TA.App/TA/Pages/Exchange/RequestExchangePage.xaml.cs
+++ 09.App/DMT.TA.App/TA/Pages/Exchange/RequestExchangePage.xaml.cs
@@ -30,5 +30,30 @@ namespace DMT.TA.Pages.Exchange
         }
 
         #endregion
+
+        #region Button Handlers
+
+        private void cmdBack_Click(object sender, RoutedEventArgs e)
+        {
+            GotoMainMenu();
+        }
+
+        private void cmdRequest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void GotoMainMenu()
+        {
+            // Main Menu Page
+            var page = new Menu.MainMenu();
+            PageContentManager.Instance.Current = page;
+        }
+
+        #endregion
     }
 }
