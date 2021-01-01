a0c28e67255269aa2431798e421b6bd8f8c21ca9
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-02 04:41:06 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #576 - DMT.TAxTODv1   - DMT.Models project.     - Add SCWAddEMV/SCWAddEMVResult class.     - Add SCWRemoveEMV/SCWRemoveEMVResult class.     - Add SCWClearEMVs/SCWClearEMVsResult class.     - Add SCWAddQRCode/SCWAddQRCodeResult class.     - Add SCWRemoveQRCode/SCWRemoveQRCodeResult class.     - Add SCWClearQRCodes/SCWClearQRCodesResult class.

--- 02.Models/DMT.Models/Models/SCW/SCWEmulators.cs
+++ 02.Models/DMT.Models/Models/SCW/SCWEmulators.cs
@@ -8,6 +8,8 @@ using NLib.Reflection;
 
 namespace DMT.Models
 {
+    #region Jobs
+
     #region SCWBOJ/SCWBOJResult
 
     #region Parameter class
@@ -160,6 +162,266 @@ namespace DMT.Models
         [PropertyMapName("status")]
         public SCWStatus status { get; set; }
     }
+
+    #endregion
+
+    #endregion
+
+    #endregion
+
+    #region EMV
+
+    #region SCWAddEMV/SCWAddEMVResult
+
+    #region Parameter Class
+
+    /// <summary>
+    /// The SCWAddEMV class
+    /// </summary>
+    public class SCWAddEMV
+    {
+        /// <summary>Gets or sets networkId.</summary>
+        [PropertyMapName("networkId")]
+        public int? networkId { get; set; }
+
+        /// <summary>Gets or sets plazaId.</summary>
+        [PropertyMapName("plazaId")]
+        public int? plazaId { get; set; }
+
+        /// <summary>Gets or sets laneId.</summary>
+        [PropertyMapName("laneId")]
+        public int? laneId { get; set; }
+
+        /// <summary>Gets or sets staffId.</summary>
+        [PropertyMapName("staffId")]
+        public string staffId { get; set; }
+
+        [PropertyMapName("staffNameTh")]
+        public string staffNameTh { get; set; }
+
+        [PropertyMapName("staffNameEn")]
+        public string staffNameEn { get; set; }
+
+        [PropertyMapName("trxDateTime")]
+        public DateTime? trxDateTime { get; set; }
+
+        [PropertyMapName("amount")]
+        public decimal? amount { get; set; }
+
+        [PropertyMapName("approvalCode")]
+        public string approvalCode { get; set; }
+
+        [PropertyMapName("refNo")]
+        public string refNo { get; set; }
+    }
+
+    #endregion
+
+    #region Result Class
+
+    /// <summary>
+    /// The SCWAddEMVResult class
+    /// </summary>
+    public class SCWAddEMVResult
+    {
+        /// <summary>Gets or sets status.</summary>
+        [PropertyMapName("status")]
+        public SCWStatus status { get; set; }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region SCWRemoveEMV/SCWRemoveEMVResult
+
+    #region Parameter class
+
+    /// <summary>
+    /// The SCWRemoveEMV class.
+    /// </summary>
+    public class SCWRemoveEMV
+    {
+        /// <summary>Gets or sets trxDateTime.</summary>
+        [PropertyMapName("trxDateTime")]
+        public DateTime? trxDateTime { get; set; }
+
+        /// <summary>Gets or sets approvalCode.</summary>
+        [PropertyMapName("approvalCode")]
+        public string approvalCode { get; set; }
+    }
+
+    #endregion
+
+    #region Result class
+
+    /// <summary>
+    /// The SCWRemoveEMVResult class
+    /// </summary>
+    public class SCWRemoveEMVResult
+    {
+        /// <summary>Gets or sets status.</summary>
+        [PropertyMapName("status")]
+        public SCWStatus status { get; set; }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region SCWClearEMVs/SCWClearEMVsResult
+
+    #region Parameter class
+
+    /// <summary>
+    /// The SCWClearEMVs class.
+    /// </summary>
+    public class SCWClearEMVs { }
+
+    #endregion
+
+    #region Result class
+
+    /// <summary>
+    /// The SCWClearEMVsResult class.
+    /// </summary>
+    public class SCWClearEMVsResult
+    {
+        /// <summary>Gets or sets status.</summary>
+        [PropertyMapName("status")]
+        public SCWStatus status { get; set; }
+    }
+
+    #endregion
+
+    #endregion
+
+    #endregion
+
+    #region QRCode
+
+    #region SCWAddQRCode/SCWAddQRCodeResult
+
+    #region Parameter Class
+
+    /// <summary>
+    /// The SCWAddQRCode class
+    /// </summary>
+    public class SCWAddQRCode
+    {
+        /// <summary>Gets or sets networkId.</summary>
+        [PropertyMapName("networkId")]
+        public int? networkId { get; set; }
+
+        /// <summary>Gets or sets plazaId.</summary>
+        [PropertyMapName("plazaId")]
+        public int? plazaId { get; set; }
+
+        /// <summary>Gets or sets laneId.</summary>
+        [PropertyMapName("laneId")]
+        public int? laneId { get; set; }
+
+        /// <summary>Gets or sets staffId.</summary>
+        [PropertyMapName("staffId")]
+        public string staffId { get; set; }
+
+        [PropertyMapName("staffNameTh")]
+        public string staffNameTh { get; set; }
+
+        [PropertyMapName("staffNameEn")]
+        public string staffNameEn { get; set; }
+
+        [PropertyMapName("trxDateTime")]
+        public DateTime? trxDateTime { get; set; }
+
+        [PropertyMapName("amount")]
+        public decimal? amount { get; set; }
+
+        [PropertyMapName("approvalCode")]
+        public string approvalCode { get; set; }
+
+        [PropertyMapName("refNo")]
+        public string refNo { get; set; }
+    }
+
+    #endregion
+
+    #region Result Class
+
+    /// <summary>
+    /// The SCWAddQRCodeResult class
+    /// </summary>
+    public class SCWAddQRCodeResult
+    {
+        /// <summary>Gets or sets status.</summary>
+        [PropertyMapName("status")]
+        public SCWStatus status { get; set; }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region SCWRemoveQRCode/SCWRemoveQRCodeResult
+
+    #region Parameter class
+
+    /// <summary>
+    /// The SCWRemoveQRCode class.
+    /// </summary>
+    public class SCWRemoveQRCode
+    {
+        /// <summary>Gets or sets trxDateTime.</summary>
+        [PropertyMapName("trxDateTime")]
+        public DateTime? trxDateTime { get; set; }
+
+        /// <summary>Gets or sets approvalCode.</summary>
+        [PropertyMapName("approvalCode")]
+        public string approvalCode { get; set; }
+    }
+
+    #endregion
+
+    #region Result class
+
+    /// <summary>
+    /// The SCWRemoveQRCodeResult class
+    /// </summary>
+    public class SCWRemoveQRCodeResult
+    {
+        /// <summary>Gets or sets status.</summary>
+        [PropertyMapName("status")]
+        public SCWStatus status { get; set; }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region SCWClearQRCodes/SCWClearQRCodesResult
+
+    #region Parameter class
+
+    /// <summary>
+    /// The SCWClearJobs class.
+    /// </summary>
+    public class SCWClearQRCodes { }
+
+    #endregion
+
+    #region Result class
+
+    /// <summary>
+    /// The SCWClearJobsResult class.
+    /// </summary>
+    public class SCWClearQRCodesResult
+    {
+        /// <summary>Gets or sets status.</summary>
+        [PropertyMapName("status")]
+        public SCWStatus status { get; set; }
+    }
+
+    #endregion
 
     #endregion
 
