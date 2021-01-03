fdf6019979b00b5911f5d0619a43142a57893de2
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-03 16:20:12 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #593 - DMT.TAxTODv1   - DMT.Plaza.Simulator.App project.     - Add UserListWindow.

---
+++ 09.App/DMT.Plaza.Simulator.App/Simulator/Windows/UserListWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using DMT.Models;
+using DMT.Services;
+using NLib.Services;
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.Simulator.Windows
+{
+    /// <summary>
+    /// Interaction logic for UserListWindow.xaml
+    /// </summary>
+    public partial class UserListWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public UserListWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+
+        }
+
+        #endregion
+    }
+}
