3594f729865c787b5cacd0ec5571e02214fbf2a6
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-06 04:32:47 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #644 - DMT.TAxTODv1   - DMT.Plaza.Simulator.App project     - Add Payment Window.

---
+++ 09.App/DMT.Plaza.Simulator.App/Simulator/Windows/PaymentWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+using DMT.Models;
+using DMT.Services;
+using NLib.Services;
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.Simulator.Windows
+{
+    /// <summary>
+    /// Interaction logic for PaymentWindow.xaml
+    /// </summary>
+    public partial class PaymentWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PaymentWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        #endregion
+    }
+}
