390593ecdf9cd2cbe5c8807e82260b9b64c60b08
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-10 04:13:55 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #744 - DMT.TAxTODv1   - DMT.Models project     - Refactor all transaction models #24.       - TSB Add property MaxCredit

--- 02.Models/DMT.Models/Models/Local/Infrastructures/TSB.cs
+++ 02.Models/DMT.Models/Models/Local/Infrastructures/TSB.cs
@@ -41,6 +41,8 @@ namespace DMT.Models
 
 		private bool _Active = false;
 
+		private decimal _MaxCredit = decimal.Zero;
+
 		#endregion
 
 		#region Constructor
@@ -163,6 +165,32 @@ namespace DMT.Models
 				{
 					_Active = value;
 					this.RaiseChanged("Active");
+				}
+			}
+		}
+
+		#endregion
+
+		#region Credit
+
+		/// <summary>
+		/// Gets or sets Max TSB Credit.
+		/// </summary>
+		[Category("Credits")]
+		[Description("Gets or sets Max TSB Credit.")]
+		[PropertyMapName("MaxCredit")]
+		public decimal MaxCredit
+		{
+			get
+			{
+				return _MaxCredit;
+			}
+			set
+			{
+				if (_MaxCredit != value)
+				{
+					_MaxCredit = value;
+					this.RaiseChanged("MaxCredit");
 				}
 			}
 		}
