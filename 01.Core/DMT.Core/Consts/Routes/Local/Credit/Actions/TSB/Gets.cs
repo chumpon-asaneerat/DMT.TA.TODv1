e48299169a9e4bb5c7ce67623e3122783feeb47e
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-09 22:14:28 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #727 - DMT.TAxTODv1   - DMT.Core project     - Add RouteConsts       - Credit Action         - Credit->TSB           - Gets         - Credit->User           - Gets           - Cancel           - Save

---
+++ 01.Core/DMT.Core/Consts/Routes/Local/Credit/Actions/TSB/Gets.cs
@@ -0,0 +1,21 @@
+﻿namespace DMT
+{
+    // Url : api/credit/tsb/gets
+    static partial class RouteConsts
+    {
+        static partial class Credit
+        {
+            static partial class TSB
+            {
+                /// <summary>The Gets TSB Credit Balance(s) action.</summary>
+                public static class Gets
+                {
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "Gets";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = TSB.Url + @"/" + Name;
+                }
+            }
+        }
+    }
+}
---
+++ 01.Core/DMT.Core/Consts/Routes/Local/Credit/Actions/User/Gets.cs
@@ -0,0 +1,21 @@
+﻿namespace DMT
+{
+    // Url : api/credit/user/gets
+    static partial class RouteConsts
+    {
+        static partial class Credit
+        {
+            static partial class User
+            {
+                /// <summary>The Gets User Credit Balance(s) action.</summary>
+                public static class Gets
+                {
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "Gets";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = User.Url + @"/" + Name;
+                }
+            }
+        }
+    }
+}
