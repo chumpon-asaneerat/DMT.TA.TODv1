aa62c9ace0e9a8f23da4e58cc4924f97a01924f3
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-11 20:40:21 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #752 - DMT.TAxTODv1   - DMT.TA.App project     - Implements Collector Revenue Entry #5.

--- 09.App/DMT.TOD.App/TOD/Pages/Revenue/CollectorRevenueEntryPage.xaml.cs
+++ 09.App/DMT.TOD.App/TOD/Pages/Revenue/CollectorRevenueEntryPage.xaml.cs
@@ -30,5 +30,71 @@ namespace DMT.TOD.Pages.Revenue
         }
 
         #endregion
+
+        #region Button Handlers
+
+        private void cmdBack_Click(object sender, RoutedEventArgs e)
+        {
+            GotoMainMenu();
+        }
+
+        private void cmdGotoRevenueEntry_Click(object sender, RoutedEventArgs e)
+        {
+            tabs.SelectedIndex = 1; // goto next tab.
+        }
+
+        private void cmdBack2_Click(object sender, RoutedEventArgs e)
+        {
+            GotoMainMenu();
+            // Used below code if need to go back to select date.
+            //GotoBack();
+        }
+
+        private void cmdGotoRevenueEntryPreview_Click(object sender, RoutedEventArgs e)
+        {
+            tabs.SelectedIndex = 2; // goto next tab.
+        }
+
+        private void cmdBack3_Click(object sender, RoutedEventArgs e)
+        {
+            GotoBack();
+        }
+
+        private void cmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            // Printing.
+
+            // Go Back to Main Menu.
+            GotoMainMenu();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void GotoMainMenu()
+        {
+            // Main Menu Page
+            var page = new Menu.MainMenu();
+            PageContentManager.Instance.Current = page;
+        }
+
+        private void GotoBack()
+        {
+            if (tabs.SelectedIndex == 0)
+            {
+                GotoMainMenu();
+            }
+            else if (tabs.SelectedIndex == 1)
+            {
+                tabs.SelectedIndex = 0;
+            }
+            else if (tabs.SelectedIndex == 2)
+            {
+                tabs.SelectedIndex = 1;
+            }
+        }
+
+        #endregion
     }
 }
